ec8dba27d39ff40ef7e471f309d6d13535d4e5bc
zhaojihui <zhaojihuionline@outlook.com>
2026-01-19 09:08:18 +0800

    no message

--- slg-game/bigmap/core/partion_actor.go
+++ slg-game/bigmap/core/partion_actor.go
@@ -11,7 +11,7 @@ import (
 	"github.com/asynkron/protoactor-go/slg-game/domain/bmap"
 )
 
-// 格子/实体 发送变化，找到所有分区，发送格子/实体的变化
+// 格子/实体 发生变化，找到所有分区，发送格子/实体的变化
 
 type PartionActor struct {
 	actor.Actor
